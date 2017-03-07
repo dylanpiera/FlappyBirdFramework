2fd8baef232aba388574cca21f6570c17b61b5f9
SoaringDylan <dylan@fampiera.nl>
2017-03-07 10:17:12 +0100

    Step 6.5 - Eigengemaakte Polish

--- FlappyBird/FlappyBird/GameOverState.cs
+++ FlappyBird/FlappyBird/GameOverState.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.Xna.Framework;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -7,6 +8,25 @@ namespace FlappyBird
 {
     class GameOverState : GameObjectList
     {
+        public GameOverState()
+        {
+            TextGameObject text = new TextGameObject("GameFont"), text2 = new TextGameObject("GameFont"), text3 = new TextGameObject("GameFont");
+            text.Text = "Press Space";
+            text2.Text = "To Continue.";
+            text3.Text = "Gameover!";
+            text.Color = Color.Black;
+            text2.Color = Color.Black;
+            text3.Color = Color.Black;
+            text.Position = new Vector2(((FlappyBird.Screen.X/2) - (text.Size.X/2)), ((FlappyBird.Screen.Y / 2)));
+            text2.Position = new Vector2(((FlappyBird.Screen.X / 2) - (text2.Size.X / 2)), ((FlappyBird.Screen.Y / 2) + (text3.Size.Y/2)));
+            text3.Position = new Vector2(((FlappyBird.Screen.X / 2) - (text3.Size.X / 2)), ((FlappyBird.Screen.Y / 2) - (text3.Size.Y)));
+
+            this.Add(new SpriteGameObject("spr_background"));
+            this.Add(text);
+            this.Add(text2);
+            this.Add(text3);
+        }
+
         public override void HandleInput(InputHelper inputHelper)
         {
             base.HandleInput(inputHelper);
