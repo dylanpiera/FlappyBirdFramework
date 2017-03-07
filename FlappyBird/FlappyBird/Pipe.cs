a2bce32ce2e5695dc048f1419d1d6e78329bb771
SoaringDylan <dylan@fampiera.nl>
2017-03-07 09:42:18 +0100

    Step 3

---
+++ FlappyBird/FlappyBird/Pipe.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.Xna.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace FlappyBird
+{
+    class Pipe : SpriteGameObject
+    {
+        public Pipe(string assetname = "spr_pipe", int layer = 0, string id = "", int sheetIndex = 0) : base(assetname, layer, id, sheetIndex)
+        {
+            this.Origin = this.Sprite.Center;
+            this.Velocity = new Vector2(-300, 0);
+            this.Position = new Vector2(700,this.RandomStart());
+        }
+
+        private int RandomStart()
+        {
+            return FlappyBird.Random.Next(200, 400);
+        }
+    }
+}
--- FlappyBird/FlappyBird/PlayingState.cs
+++ FlappyBird/FlappyBird/PlayingState.cs
@@ -2,22 +2,52 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using Microsoft.Xna.Framework;
 
 namespace FlappyBird
 {
     class PlayingState : GameObjectList
     {
         private Bird bird;
+        private GameObjectList pipes;
+        private int frameCount;
+
+        public GameObjectList Pipes
+        {
+            get
+            {
+                return pipes;
+            }
+
+            set
+            {
+                pipes = value;
+            }
+        }
 
         public PlayingState()
         {
             bird = new Bird();
-
+            pipes = new GameObjectList();
+            frameCount = 0;
 
 
 
             this.Add(new SpriteGameObject("spr_background"));
             this.Add(bird);
+            this.Add(pipes);
+        }
+
+        public override void Update(GameTime gameTime)
+        {
+            base.Update(gameTime);
+            frameCount++;
+
+            if(frameCount >= 100)
+            {
+                frameCount = 0;
+                this.Pipes.Add(new Pipe());
+            }
         }
     }
 }
